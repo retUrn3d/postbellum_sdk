8d1f843cb913359b141ae2350e6bdf093874efa8
retUrn3d <36128194+retUrn3d@users.noreply.github.com>
2022-02-07 23:51:06 +0500

    Update sh_craft_bandage.lua

--- gamemodes/postbellum/framework/recipes/sh_craft_bandage.lua
+++ gamemodes/postbellum/framework/recipes/sh_craft_bandage.lua
@@ -2,7 +2,7 @@ local RECIPE = Clockwork.recipe:New()
 	RECIPE.name = "Бинт"
 	RECIPE.category = "Медикаменты"
 	RECIPE.description = "Изготовить бинт из обрывка ткани"
-	RECIPE.model = "models/props_junk/watermelon01_chunk01c.mdl"
+	RECIPE.model = "models/pb_upd/medical/bandage.mdl"
 	RECIPE.required = {
 		{"scrap_cloth", 1}
 	}
