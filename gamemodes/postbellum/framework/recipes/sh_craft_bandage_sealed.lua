52c482ec80d23bd8a6d4c74ca5002305ca3bb534
retUrn3d <36128194+retUrn3d@users.noreply.github.com>
2022-07-01 02:10:50 +0500

    Изменение крафта
    Теперь требуется навык для крафта(нужно ведь еще быть чуть умнее чтобы продезинфицировать бинт!)
    Добавлен выбор чем дезинфицировать.

--- gamemodes/postbellum/framework/recipes/sh_craft_bandage_sealed.lua
+++ gamemodes/postbellum/framework/recipes/sh_craft_bandage_sealed.lua
@@ -1,12 +1,19 @@
 local RECIPE = Clockwork.recipe:New()
 	RECIPE.name = "Стерильный бинт"
 	RECIPE.category = "Медикаменты"
-	RECIPE.description = "Продезинфицировать бинт"
+	RECIPE.description = "Продезинфицированный бинт"
 	RECIPE.skin = 1
 	RECIPE.model = "models/pb_upd/medical/bandage.mdl"
+	RECIPE.reqAtts = { 
+		{"dex", 2},
+		{"med", 4},
+	}
 	RECIPE.required = {
 		{"bandage"},
-		{"alcohol"},
+		{
+			{"vodka"},
+			{"alcohol"},
+		}
 	}
 	RECIPE.output = {
 		{"bandage_sealed", 1}
