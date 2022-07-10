f69d7da5a8892e7edda94b7fc802ed3ec5b20a8a
retUrn3d <36128194+retUrn3d@users.noreply.github.com>
2022-07-10 17:25:49 +0500

    Крафты с лимоном
    -Добавлен способ изготовления чая с лимоном;
    -Добавлен способ изготовления лимонада.

---
+++ gamemodes/postbellum/framework/recipes/sh_make_lemon_tea.lua
@@ -0,0 +1,17 @@
+local RECIPE = Clockwork.recipe:New()
+	RECIPE.name = "Заварить чай с лимоном"
+	RECIPE.category = "Еда"
+	RECIPE.description = "Лучше обычного чая, может быть только чай лимоном."
+	RECIPE.model = "models/uu_branded/probs_misc/tobbcco_box-1.mdl"
+	RECIPE.required = {
+		{"uu_tea"},
+		{"lemon"},
+		{
+			{"breens_water", 3},
+			{"water", 3},
+		}
+	}
+	RECIPE.output = {
+		{"lemon_tea_cup", 3}
+	}
+RECIPE:Register()
---
+++ gamemodes/postbellum/framework/recipes/sh_make_lemonade.lua
@@ -0,0 +1,18 @@
+local RECIPE = Clockwork.recipe:New()
+	RECIPE.name = "Лимонад"
+	RECIPE.category = "Еда"
+	RECIPE.description = "Газировка смешанная с дольками лимона и слегка разбавленная обычной водой."
+	RECIPE.model = "models/props_interiors/pot01a.mdl"
+	RECIPE.required = {
+		{"lemon", 1},
+		{"large_soda_empty", 2}
+		{"smooth_breens_water", 2}
+		{
+			{"breens_water", 1},
+			{"water", 1},
+		}
+	}
+	RECIPE.output = {
+		{"lemonade", 2}
+	}
+RECIPE:Register()
