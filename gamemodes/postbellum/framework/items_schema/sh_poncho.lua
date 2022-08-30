25aee392843d2f671f8755e2e3057d767f26c037
retUrn3d <36128194+retUrn3d@users.noreply.github.com>
2022-08-30 19:42:40 +0500

    Изменение моделей
    Обычное пончо теперь имеет корректную модель, как и черное.

--- gamemodes/postbellum/framework/items_schema/sh_poncho.lua
+++ gamemodes/postbellum/framework/items_schema/sh_poncho.lua
@@ -1,14 +1,9 @@
---[[
-	© 2013 CloudSixteen.com do not share, re-distribute or modify
-	without permission of its author (kurozael@gmail.com).
---]]
-
 local ITEM = Clockwork.item:New("cits_base");
 ITEM.name = "Пончо";
 ITEM.plural = "Пончо";
 ITEM.model = "models/hl2rp/vortigaunt_pb/suit/pb_vort_poncho.mdl"
 ITEM.weight = 2;
-ITEM.protection = 0.3;
+ITEM.protection = 0.2;
 ITEM.description = "Больше похоже на старую тряпку, но Вортигонтам нравится.";
 ITEM.wearBodyId = 5
 ITEM.wearBodyState = 7
--- gamemodes/postbellum/framework/items_schema/sh_poncho_black.lua
+++ gamemodes/postbellum/framework/items_schema/sh_poncho_black.lua
@@ -1,9 +1,9 @@
 local ITEM = Clockwork.item:New("cits_base");
 ITEM.name = "Черное пончо";
 ITEM.plural = "Черных пончо";
-ITEM.model = "models/hl2rp/vortigaunt_pb/suit/pb_vort_poncho.mdl"
+ITEM.model = "models/hl2rp/vortigaunt_pb/suit/pb_vort_poncho1.mdl"
 ITEM.weight = 2;
-ITEM.protection = 0.3;
+ITEM.protection = 0.2;
 ITEM.description = "Больше похоже на старую тряпку, которую замочили в черной краске, но Вортигонтам нравится.";
 ITEM.wearBodyId = 5;
 ITEM.wearBodyState = 8;
