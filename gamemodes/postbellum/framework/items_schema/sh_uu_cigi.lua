24eeb0d5d7f57765a897a4d234f2574e0acbf5e3
Shararvev <shararvev@gmail.com>
2022-10-03 00:22:40 +0500

    pb_v1557 * Animation. Классы моделей определяются автоматически на основе SubModels. Это происходит в реальном времени по мере необходимости. Все модели кешируются, даже если не удалось найти класс. GetModelClass теперь принимает энтить. Удалил все захардкоданные модели игроков, неиспользуемые GetMenuSequence и AddOverride.

--- gamemodes/postbellum/framework/items_schema/sh_uu_cigi.lua
+++ gamemodes/postbellum/framework/items_schema/sh_uu_cigi.lua
@@ -13,28 +13,28 @@ ITEM.vomit = 15;
 -- Called when a player uses the item.
 function ITEM:OnUse(player, itemEntity)
 	player:SetCharacterData("Stamina", 0);
-	
+
 	player:BoostAttribute(self.name, ATB_ACROBATICS, -10, 120);
 	player:BoostAttribute(self.name, ATB_ENDURANCE, -10, 120);
 	player:BoostAttribute(self.name, ATB_STRENGTH, -10, 120);
 
-	local modelClass = Clockwork.animation:GetModelClass(player:GetModel());
+	local modelClass = Clockwork.animation:GetModelClass(player);
 	local att = player:LookupAttachment("eyes");
 	local id = "Smoke"..player:EntIndex();
-	
+
 	if (modelClass == "maleHuman" or modelClass == "femaleHuman") then
 		att = player:LookupAttachment("mouth")
 	end;
-	
+
 	ParticleEffectAttach("smoke_exhaust_01b", PATTACH_POINT_FOLLOW, player, att)
 	player:EmitSound("ambient/wind/wind_snippet4.wav", 60, 150)
-	
+
 	timer.Create(id, 15, 5, function()
 		if !IsValid(player) then
 			timer.Remove(id)
 			return
 		end
-		
+
 		ParticleEffectAttach("smoke_exhaust_01b", PATTACH_POINT_FOLLOW, player, att)
 		player:EmitSound("ambient/wind/wind_snippet4.wav", 60, 150)
 	end)
